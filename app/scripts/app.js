2d50fb60f1b2d2ef6370dadc31f378f8b878a4de
Cudderfelix <ifeachochukafelix@gmail.com>
2022-02-08 06:03:58 +0100

    freddieFinalProjectv1.2.2

---
+++ app/scripts/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const ejs = require('ejs');
+
+// express app
+const app = express();
+
+
+// register view engine    
+app.set('view engine', 'ejs');
+
+// listen for requests
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 8000;
+}
+app.listen(port);
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+
+    res.render('landing', { title: 'Welcome' });
+});
+
+app.get('/index.html', (req, res) => {
+    res.render('index', { title: 'Home' });
+});
+
+app.use((req, res) => {
+    res.render('404', { title: '404 Error' });
+});
+
+
+
+
+//app.get('/about-us', (req, res) => {
+//    res.redirect('/about');
+//});
